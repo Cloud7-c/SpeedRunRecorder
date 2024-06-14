0896e7a2d6b41067361f6705da02aa95a016ebdb
Cloud7-c <Cloud7_c@outlook.com>
2024-06-15 03:18:24 +0800

    optimize log

--- src/main/java/cloud7c/speedrunrecorder/util/Logger.java
+++ src/main/java/cloud7c/speedrunrecorder/util/Logger.java
@@ -2,6 +2,6 @@ package cloud7c.speedrunrecorder.util;
 
 public class Logger {
     public static void log(String message){
-        System.out.println(message);
+        System.out.println("[" + TimeUtil.formatDateTime(System.currentTimeMillis()) + "] " + message);
     }
 }
