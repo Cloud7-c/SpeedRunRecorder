d5ff58c40ec0cd712a9a09104f76caf594a22880
Cloud7-c <Cloud7_c@outlook.com>
2024-06-15 03:18:11 +0800

    optimize time format

--- src/main/java/cloud7c/speedrunrecorder/util/TimeUtil.java
+++ src/main/java/cloud7c/speedrunrecorder/util/TimeUtil.java
@@ -8,6 +8,7 @@ import java.time.format.DateTimeFormatter;
 public class TimeUtil {
     public static final String TIME_FORMAT = "%02d:%02d";
     public static final DateTimeFormatter DATE_TIME_FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+    public static final DateTimeFormatter Date_TIME_WITHOUT_YEAR_FORMATTER = DateTimeFormatter.ofPattern("MM-dd HH:mm:ss");
 
     public static String formatTime(long milliseconds){
         long seconds = milliseconds / 1000 % 60;
@@ -16,6 +17,15 @@ public class TimeUtil {
     }
 
     public static String formatDateTime(long milliseconds) {
+        Instant instant = Instant.ofEpochMilli(milliseconds);
+        LocalDateTime dateTime = LocalDateTime.ofInstant(instant, ZoneId.systemDefault());
+        return dateTime.format(Date_TIME_WITHOUT_YEAR_FORMATTER);
+    }
+
+    public static String formatDateTime(long milliseconds, boolean withYear) {
+        if(!withYear){
+            return formatDateTime(milliseconds);
+        }
         Instant instant = Instant.ofEpochMilli(milliseconds);
         LocalDateTime dateTime = LocalDateTime.ofInstant(instant, ZoneId.systemDefault());
         return dateTime.format(DATE_TIME_FORMATTER);
